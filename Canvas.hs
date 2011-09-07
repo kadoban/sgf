d72694bbdaa2c19f45633570cb890aa6317a7f61
Joshua Simmons <joshua.simmons@emptypath.com>
2011-09-07 00:06:22 -0700

    might as well do all the board lines in one go

--- Canvas.hs
+++ Canvas.hs
@@ -159,13 +159,10 @@ createBoard scSz@(scw, sch) v@((lx, ly), (gx, gy)) (bw, bh) =
                C.setAntialias C.AntialiasNone
                C.setLineWidth 1
                C.setLineCap C.LineCapSquare
-               foldl1 (>>) $ map drawLinex [lx+1..gx-1]
-               foldl1 (>>) $ map drawLiney [ly+1..gy-1]
-               let edges = [(boardEdgex lx, drawLinex lx), -- this is slightly gross,
-                            (boardEdgex gx, drawLinex gx), -- but we need to sort these
-                            (boardEdgey ly, drawLiney ly), -- by if they're an edge or not
-                            (boardEdgey gy, drawLiney gy)] -- and do the edges last
-               foldl1 (>>) $ map snd $ sortBy (comparing fst) edges
+               let xlines = map (\x -> (boardEdgex x, drawLinex x)) [lx..gx]
+               let ylines = map (\y -> (boardEdgey y, drawLiney y)) [ly..gy]
+               -- draw the board edges last, so they don't get overwritten
+               foldl1 (>>) $ map snd $ sortBy (comparing fst) $ xlines ++ ylines
 
 drawStone col pnt (m, ssz) =
     do if col == Black then C.setSourceRGB 0 0 0 else C.setSourceRGB 1 1 1
