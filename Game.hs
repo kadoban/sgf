bb5bab4a43561e94a1b8d576fdbc53eef7d528a8
Joshua Simmons <joshua.simmons@emptypath.com>
2011-09-14 13:12:41 -0700

    clean up applyProperty function a bit
    still a little grosstastic, but better

--- Game.hs
+++ Game.hs
@@ -71,36 +71,33 @@ peekToPlay g =
 
 applyProperty :: Game -> Property -> Game
 applyProperty game prop =
-    let game' = game{applied = prop : (applied game)} in
+    let g = game{applied = prop : (applied game)}
+        b = board game
+        m = marks game in
     case prop of
-        BoardSize sz  -> game'{board = f (board game')}
-            where f b = b{size = sz}
-        Add c ps      -> game'{board = f (board game')}
-            where f b = b{stones = stones'}
-                      where stones' = foldl' insStone (stones b) ps
-                            insStone = flip (c `ins'`)
-                            ins' = flip M.insert
-        Clear ps      -> game'{board = f (board game')}
-            where f b = b{stones = stones'}
-                      where stones' = foldl' (flip M.delete) (stones b) ps
-        Move c p      -> game'{board = f (board game'),
-                             moveNum = (+1) (moveNum game'),
-                             toPlay = other c}
-            where f b = b'''
-                      where b'   = b{stones = M.insert p c (stones b)}
-                            b''  = foldl' captureLifeless b' (neighbors b' p)
-                            b''' = captureLifeless b'' p
-        View ps       -> game'{view = ps}
-        Comment s     -> game'{comment = s}
-        a@(Mark t ps) -> game'{marks = f (marks game')}
-            where f m = insertMarks m a
-        Labels ps     -> game'{marks = f (marks game')}
-            where f m = foldl' insertMarks (marks game') (map toMark ps)
+        BoardSize sz  -> g{board = b{size = sz}}
+        Add c ps      -> g{board = b{stones = stones'}}
+            where stones' = foldl' insStone (stones b) ps
+                  insStone = flip (c `ins'`)
+                  ins' = flip M.insert
+        Clear ps      -> g{board = b{stones = stones'}}
+            where stones' = foldl' (flip M.delete) (stones b) ps
+        Move c p      -> g{board = b''',
+                           moveNum = (moveNum g) + 1,
+                           toPlay = other c}
+            where b'   = b{stones = M.insert p c (stones b)}
+                  b''  = foldl' captureLifeless b' (neighbors b' p)
+                  b''' = captureLifeless b'' p
+        View ps       -> g{view = ps}
+        Comment s     -> g{comment = s}
+        a@(Mark {})   -> g{marks = insertMarks m a}
+        Labels ps     -> g{marks = m'}
+            where m' = foldl' insertMarks m (map toMark ps)
                   toMark (p, s) = Mark (L s) [p]
-        ToPlay c      -> game'{toPlay = c}
-        Handicap n | n >= 2 && (moveNum game') == 0 -> game' {toPlay = White}
-                   | otherwise -> game'
-        otherwise     -> game'
+        ToPlay c      -> g{toPlay = c}
+        Handicap n | n >= 2 && (moveNum g) == 0 -> g {toPlay = White}
+                   | otherwise -> g
+        otherwise     -> g
 
 insertMarks :: (M.Map Point MarkType) -> Property -> (M.Map Point MarkType)
 insertMarks m (Mark t ps) = foldl' insM m ps
