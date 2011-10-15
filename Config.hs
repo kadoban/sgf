38609a752f8fa7bbf2fdefd4a9707445342730cc
Joshua Simmons <joshua.simmons@emptypath.com>
2011-10-15 12:38:01 -0400

    change configured directory

--- Config.hs
+++ Config.hs
@@ -3,4 +3,4 @@ module Config where
 
 screenSpace = (155, 180)
 
-sgfDirectory = "/var/www/usgo.org/drupal/sites/default/files/weekly_problem/"
+sgfDirectory = "/var/www/usgo.org/files/weekly_problem/"
